8e9e30c52c23c2582891d321e1b22986a8559b82
Donald Sharp <sharpd@cumulusnetworks.com>
2018-02-05 10:26:44 -0500

    pbrd: Add event subsystem
    Add skeleton structure to allow pbrd to translate
    cli and nexthop changes into events it can handle
    at a somewhat later time.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

---
+++ pbrd/pbr_event.c
@@ -0,0 +1,56 @@
+/*
+ * PBR-event Code
+ * Copyright (C) 2018 Cumulus Networks, Inc.
+ *               Donald Sharp
+ *
+ * This file is part of FRR.
+ *
+ * FRR is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * FRR is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; see the file COPYING; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+#include <zebra.h>
+
+#include <thread.h>
+#include <workqueue.h>
+#include <nexthop.h>
+#include <log.h>
+
+#include "pbrd/pbr_event.h"
+
+struct work_queue *pbr_event_wq;
+
+static void pbr_event_delete_wq(struct work_queue *wq, void *data)
+{
+	return;
+}
+
+static wq_item_status pbr_event_process_wq(struct work_queue *wq, void *data)
+{
+	return WQ_SUCCESS;
+}
+
+extern struct thread_master *master;
+
+void pbr_event_init(void)
+{
+	pbr_event_wq = work_queue_new(master, "PBR Main Work Queue");
+	pbr_event_wq->spec.workfunc = &pbr_event_process_wq;
+	pbr_event_wq->spec.del_item_data = &pbr_event_delete_wq;
+}
+
+void pbr_event_stop(void)
+{
+	work_queue_free(pbr_event_wq);
+	pbr_event_wq = NULL;
+}
---
+++ pbrd/pbr_event.h
@@ -0,0 +1,44 @@
+/*
+ * PBR-event Header
+ * Copyright (C) 2018 Cumulus Networks, Inc.
+ *               Donald Sharp
+ *
+ * This file is part of FRR.
+ *
+ * FRR is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * FRR is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; see the file COPYING; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+#ifndef __PBR_EVENT_H__
+#define __PBR_EVENT_H__
+
+enum pbr_events {
+	PBR_NHG_ADD,
+	PBR_NHG_MODIFY,
+	PBR_NHG_DELETE,
+	PBR_MAP_ADD,
+	PBR_MAP_MODIFY,
+	PBR_MAP_DELETE,
+	PBR_NH_CHANGED,
+};
+
+struct pbr_event {
+	enum pbr_events event;
+
+	char name[100];
+	union g_addr addr;
+};
+
+extern void pbr_event_init(void);
+extern void pbr_event_stop(void);
+#endif
