2ec8932343f8db0ee0e575c26710a06d8e8508c9
Donald Sharp <sharpd@cumulusnetworks.com>
2018-02-07 10:15:51 -0500

    vtysh: Fix 'show work-queues...' for pbrd.
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- vtysh/vtysh.c
+++ vtysh/vtysh.c
@@ -1966,7 +1966,7 @@ DEFUN (vtysh_show_work_queues,
 
 DEFUN (vtysh_show_work_queues_daemon,
        vtysh_show_work_queues_daemon_cmd,
-       "show work-queues <zebra|ripd|ripngd|ospfd|ospf6d|bgpd|isisd>",
+       "show work-queues <zebra|ripd|ripngd|ospfd|ospf6d|bgpd|isisd|pbrd>",
        SHOW_STR
        "Work Queue information\n"
        "For the zebra daemon\n"
@@ -1975,7 +1975,8 @@ DEFUN (vtysh_show_work_queues_daemon,
        "For the ospf daemon\n"
        "For the ospfv6 daemon\n"
        "For the bgp daemon\n"
-       "For the isis daemon\n")
+       "For the isis daemon\n"
+       "For the pbr daemon\n")
 {
 	int idx_protocol = 2;
 	unsigned int i;
