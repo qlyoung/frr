43f2d3355899e5e7909d3baa4c6f2b8a07b99dd7
Quentin Young <qlyoung@cumulusnetworks.com>
2018-07-05 21:30:43 +0000

    Revert "ospfd: Memory asserts on failure to create no need to check"
    Reverting this commit as there are cases where the NULL checks are
    necessary.
    
    This reverts commit 6c489052971de436d5ef9b656b5aea87d85ff2ad.

--- ospfd/ospf_te.c
+++ ospfd/ospf_te.c
@@ -1146,7 +1146,10 @@ static struct ospf_lsa *ospf_mpls_te_lsa_new(struct ospf *ospf,
 	uint16_t length;
 
 	/* Create a stream for LSA. */
-	s = stream_new(OSPF_MAX_LSA_SIZE);
+	if ((s = stream_new(OSPF_MAX_LSA_SIZE)) == NULL) {
+		zlog_warn("ospf_mpls_te_lsa_new: stream_new() ?");
+		return NULL;
+	}
 	lsah = (struct lsa_header *)STREAM_DATA(s);
 
 	options = OSPF_OPTION_O; /* Don't forget this :-) */
@@ -1198,8 +1201,18 @@ static struct ospf_lsa *ospf_mpls_te_lsa_new(struct ospf *ospf,
 	lsah->length = htons(length);
 
 	/* Now, create an OSPF LSA instance. */
-	new = ospf_lsa_new();
-	new->data = ospf_lsa_data_new(length);
+	if ((new = ospf_lsa_new()) == NULL) {
+		zlog_warn("%s: ospf_lsa_new() ?", __func__);
+		stream_free(s);
+		return NULL;
+	}
+	if ((new->data = ospf_lsa_data_new(length)) == NULL) {
+		zlog_warn("%s: ospf_lsa_data_new() ?", __func__);
+		ospf_lsa_unlock(&new);
+		new = NULL;
+		stream_free(s);
+		return new;
+	}
 
 	new->vrf_id = ospf->vrf_id;
 	if (area && area->ospf)
