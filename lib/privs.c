be1858b490d289ef5d8d12c64e66dc2286f2220b
Quentin Young <qlyoung@cumulusnetworks.com>
2020-01-02 02:01:57 -0500

    lib: improve frr_init_fast() speeeed
    Aggressively disable some more things.
    
    This also disables privilege dropping, because it breaks AFL deferred
    mode. After this patch we can run about 10-20x faster with deferred
    mode!
    
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- lib/privs.c
+++ lib/privs.c
@@ -277,6 +277,7 @@ static void zprivs_caps_init(struct zebra_privs_t *zprivs)
 	/* we have caps, we have no need to ever change back the original user
 	 */
 	/* only change uid if we don't have the correct one */
+#ifndef FUZZING
 	if ((zprivs_state.zuid) && (zprivs_state.zsuid != zprivs_state.zuid)) {
 		if (setreuid(zprivs_state.zuid, zprivs_state.zuid)) {
 			fprintf(stderr,
@@ -285,6 +286,7 @@ static void zprivs_caps_init(struct zebra_privs_t *zprivs)
 			exit(1);
 		}
 	}
+#endif
 
 	if (!(zprivs_state.caps = cap_init())) {
 		fprintf(stderr, "privs_init: failed to cap_init, %s\n",
@@ -315,6 +317,7 @@ static void zprivs_caps_init(struct zebra_privs_t *zprivs)
 	/* apply caps. CAP_EFFECTIVE is cleared. we'll raise the caps as
 	 * and when, and only when, they are needed.
 	 */
+#ifndef FUZZING
 	if (cap_set_proc(zprivs_state.caps)) {
 		cap_t current_caps;
 		char *current_caps_text = NULL;
@@ -341,6 +344,7 @@ static void zprivs_caps_init(struct zebra_privs_t *zprivs)
 
 		exit(1);
 	}
+#endif
 
 	/* set methods for the caller to use */
 	zprivs->change = zprivs_change_caps;
@@ -478,6 +482,9 @@ static struct zebra_privs_refs_t *get_privs_refs(struct zebra_privs_t *privs)
 struct zebra_privs_t *_zprivs_raise(struct zebra_privs_t *privs,
 				    const char *funcname)
 {
+#ifdef FUZZING
+	return NULL;
+#endif
 	int save_errno = errno;
 	struct zebra_privs_refs_t *refs;
 
@@ -508,6 +515,9 @@ struct zebra_privs_t *_zprivs_raise(struct zebra_privs_t *privs,
 
 void _zprivs_lower(struct zebra_privs_t **privs)
 {
+#ifdef FUZZING
+	return;
+#endif
 	int save_errno = errno;
 	struct zebra_privs_refs_t *refs;
 
@@ -652,6 +662,7 @@ void zprivs_init(struct zebra_privs_t *zprivs)
 
 	zprivs_state.zsuid = geteuid(); /* initial uid */
 	/* add groups only if we changed uid - otherwise skip */
+#ifndef FUZZING
 	if ((ngroups) && (zprivs_state.zsuid != zprivs_state.zuid)) {
 		if (setgroups(ngroups, groups)) {
 			fprintf(stderr, "privs_init: could not setgroups, %s\n",
@@ -669,6 +680,7 @@ void zprivs_init(struct zebra_privs_t *zprivs)
 			exit(1);
 		}
 	}
+#endif
 
 #ifdef HAVE_CAPABILITIES
 	zprivs_caps_init(zprivs);
@@ -694,6 +706,7 @@ void zprivs_init(struct zebra_privs_t *zprivs)
 	 */
 	zprivs_state.zsuid = geteuid();
 	/* only change uid if we don't have the correct one */
+#ifndef FUZZING
 	if ((zprivs_state.zuid) && (zprivs_state.zsuid != zprivs_state.zuid)) {
 		if (setreuid(-1, zprivs_state.zuid)) {
 			fprintf(stderr,
@@ -705,6 +718,7 @@ void zprivs_init(struct zebra_privs_t *zprivs)
 
 	zprivs->change = zprivs_change_uid;
 	zprivs->current_state = zprivs_state_uid;
+#endif
 #endif /* HAVE_CAPABILITIES */
 }
 
